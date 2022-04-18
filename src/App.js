4de9c2e9ddde766ef04a818ca2c3515c9dfb6f6b
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-04-18 17:39:24 -0400

    Setting up and creating Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes,Route, Link} from 'react-router-dom'
+import Services from './components/Services';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Error from './components/Error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return( 
+  <Router>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/services'>Services</Link>
+      <Link to='/contact'>Contact</Link>
+    </nav>
+    <Routes>
+<Route path='/' element={<Home/>} />
+<Route path='/services' element={<Services />} />
+<Route path='/contact' element={<Contact />} />
+<Route path='*' element={<Error />}/>
+  </Routes>
+
+  </Router>
   );
 }
 
