e2c0c7b41b505828fffccf7c96e71d98e6c16099
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-04-25 18:47:45 -0400

    Listed services

--- src/components/Services.js
+++ src/components/Services.js
@@ -2,10 +2,66 @@ import React from "react";
 import rounded from './images/rounded.svg'
 const Services = () => {
     return(
+     
+       
+      
         <div>
-        <img className='services-round' src={rounded} alt="logo" /> 
-       
-       
+            
+            {/* <img className='services-round' src={rounded} alt="logo"  /> */}
+        <div className='spins'>
+            <h1 className='title'>Radio Spins</h1>
+           <li>Hip Hop Weekly Radio </li>
+        </div>
+        <div className='placement'>
+            <h1 className='title'>Music Video Placement </h1>
+            <ul>
+                <li>Apple Music, Tidal, and VEVO $950</li>
+                <li>Video on BET Jams/MTV Jams/Revolt $1200 (refundable if it doesn't get picked up has to be clean</li>
+            </ul>
+        </div>
+        <div className='consultation'>
+            <h1 className='title'>Artist Consultation</h1>
+            <ul>
+                <li>Review Social Media Platforms</li>
+                <li>Two Song Music Review </li>
+                <li>Review of Artist's Image </li>
+                <li>One hour Phone Consult $200</li>
+                <li>Two Hour In-Studio/In-Person Consult $350</li>
+            </ul>
+        </div>
+        <div className='art-pr'>
+            <h1 className='title'>Artist PR Package - $900</h1>
+            <ul>
+                <li>Promotional Material and Ads</li>
+                <li>Social Media Boost (Followers, Engagement, Likes, Views and More)</li>
+                <li>PR Package (Major Magazine and Blog Placement)  </li>
+                </ul>
+        </div>
+        <div className='artd-pr'>
+            <h1 className='title'>Artist Development and PR Package  - $1800</h1>
+            <ul>
+                <li>Promotional Material and Ads</li>
+                <li>Social Media Boost (Followers, Engagement, Likes, Views and More)</li>
+                <li>PR Package (Major Magazine and Blog Placement)  </li>
+                <li>Sit Down Interview  </li>
+                <li>Photo shoot with styling direction   </li>
+                <li>Interview with major magazine   </li>
+                </ul>
+        </div>
+        <div className='artd-pr-prem'>
+            <h1 className='title'>Artist Development and PR Premium - $3,500</h1>
+            <ul>
+                <li>Promotional Material and Ads</li>
+                <li>Social Media Boost (Followers, Engagement, Likes, Views and More)</li>
+                <li>PR Package (Major Magazine and Blog Placement)  </li>
+                <li>Sit Down Interview  </li>
+                <li>Photo shoot with styling direction   </li>
+                <li>Interview with major magazine   </li>
+                <li>Two Hour Studio session with Major Label Record Rep  </li>
+                <li>Email blast to over 2,000 industry contacts  </li>
+                <li>One Booked Artist Showcase  </li>
+                </ul>
+        </div>
        
         </div>
     );
